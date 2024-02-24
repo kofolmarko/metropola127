bd3d157aed61b754ca1a8837d58e23ebd8265d4d
mk21857 <mk21857@student.uni-lj.si>
2024-02-24 15:09:22 +0100

    Modules and child routing setup.

--- src/app/modules/backbone/backbone-routing.module.ts
+++ src/app/modules/backbone/backbone-routing.module.ts
@@ -3,7 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
 
 const routes: Routes = [
-    { path: '', component: NavigationComponent },
+    { 
+        path: '',
+        component: NavigationComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('../home/home.module').then(m => m.HomeModule)
+            },
+            {
+                path: 'team',
+                loadChildren: () => import('../team/team.module').then(m => m.TeamModule)
+            },
+            {
+                path: 'projects',
+                loadChildren: () => import('../projects/projects.module').then(m => m.ProjectsModule)
+            }
+        ]
+    },
 ];
 
 @NgModule({
